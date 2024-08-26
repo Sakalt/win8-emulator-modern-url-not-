85ce1c9ff2d9e7c394375f0992b4b81a2176cdfa
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-08-26 12:29:25 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		outDir: 'public' // 出力ディレクトリを 'public' に設定
-	}
+  plugins: [sveltekit()],
+  build: {
+    outDir: 'build', // 出力ディレクトリを 'build' に設定
+  },
 });
