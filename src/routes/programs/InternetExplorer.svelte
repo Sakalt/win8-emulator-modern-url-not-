cf3e66d6cb0802e91c7ead161c2fa594105f6518
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-08-26 13:18:06 +0900

    Update InternetExplorer.svelte

--- src/routes/programs/InternetExplorer.svelte
+++ src/routes/programs/InternetExplorer.svelte
@@ -3,12 +3,15 @@
     import WindowBase from '../components/WindowBase.svelte';
     import '../webpage.css';
     import { onMount } from 'svelte';
+    import { TaskManager, type IProgramManager, CommandManager, WebsiteStage } from '../stores';
 
     let width = 1200;
     let height = 700;
-
     let program: ComputerProgram = ProgramFilter.Find('ie9')!;
-    let iframeSrc: string = "https://www.bing.com"; // 初期URLとしてBingを設定
+
+    // 初期URLをBingに設定
+    let iframeSrc: string = "https://www.bing.com";
+    // 入力フォーム用のURL
     let newUrl: string = "";
 
     function updateIframeSrc() {
@@ -25,8 +28,15 @@
         style={$WebsiteStage != 3 ? 'height:100%;' : 'height:fit-content;'}
     >
         {#if $WebsiteStage == 3}
-            <!-- iframeとURL入力フォーム -->
             <iframe src={iframeSrc} width="100%" height="100%" style="border:none;"></iframe>
+        {/if}
+
+        {#if $WebsiteStage != 3}
+            <!-- コンテンツがない場合の表示を削除 -->
+        {/if}
+
+        {#if $WebsiteStage == 3}
+            <!-- 入力フォームとボタンを追加 -->
             <div class="url-input">
                 <input 
                     type="text" 
@@ -34,7 +44,7 @@
                     bind:value={newUrl} 
                     style="width: calc(100% - 110px); padding: 5px;"
                 />
-                <button on:click={updateIframeSrc} style="padding: 5px 10px;">Go</button>
+                <button on:click={updateIframeSrc} style="padding: 5px 10px;">Update URL</button>
             </div>
         {/if}
     </div>
